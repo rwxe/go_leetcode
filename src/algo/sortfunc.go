77754a78939ff570462d39697b5aa58af7e29cba
yizdu <jzunk@protonmail.com>
2021-05-29 22:05:38 +0800

    feat(*):删除交换文件

--- src/algo/sortfunc.go
+++ src/algo/sortfunc.go
@@ -2,16 +2,6 @@ package algo
 
 //var a =[]int{9,3,41,7,98,621,1,4}
 
-func MergeSort(nums []int)[]int{
-	if len(nums)<=1{
-		return nums
-	}
-	mid:=len(nums)/2
-	left:=MergeSort(nums[:mid])
-	right:=MergeSort(nums[mid:])
-	result:=merge(left,right)
-	return result
-}
 
 func merge(left,right []int)[]int{
 	l:=0
@@ -29,4 +19,46 @@ func merge(left,right []int)[]int{
 	result=append(result,left[l:]...)
 	result=append(result,right[r:]...)
 	return result
+}
+func MergeSort(arr []int)[]int{
+	if len(arr)<=1{
+		return arr
+	}
+	mid:=len(arr)/2
+	left:=MergeSort(arr[:mid])
+	right:=MergeSort(arr[mid:])
+	result:=merge(left,right)
+	return result
+}
+
+func QuickSort0(arr []int,leftEnd,rightEnd int){
+	if leftEnd>rightEnd{
+		return 
+	}
+	left:=leftEnd
+	right:=rightEnd
+	pivot:=arr[left]
+	for left<right{
+		for left<right && arr[right]>=pivot{
+			right-=1
+		}
+		if left<right{
+			arr[left]=arr[right]
+		}
+		for left<right && arr[left]<=pivot{
+			left+=1
+		}
+		if left<right{
+			arr[right]=arr[left]
+		}
+		if left>=right{
+			arr[left]=pivot
+		}
+	}
+	QuickSort0(arr,leftEnd,right-1)
+	QuickSort0(arr,left+1,rightEnd)
+}
+
+func QuickSort1(arr[]int,leftEnd,rightEnd int){
+	if 
 }
