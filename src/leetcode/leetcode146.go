5e5a2231bde1e0fa6edb92b0547b71d9f5f00fc4
yizdu <jzunk@protonmail.com>
2021-06-06 16:57:13 +0800

    *

---
+++ src/leetcode/leetcode146.go
@@ -0,0 +1,109 @@
+package leetcode
+
+import "fmt"
+
+
+type LRUDoubleListNode struct {
+	prev, next *LRUDoubleListNode
+	Key, Value int
+}
+
+type LRUDoubleList struct {
+	Head, Tail *LRUDoubleListNode
+}
+
+func NewLRUDoubleList() LRUDoubleList {
+	l := LRUDoubleList{}
+	l.Head = &LRUDoubleListNode{}
+	l.Tail = &LRUDoubleListNode{}
+	l.Head.next = l.Tail
+	l.Tail.prev = l.Head
+	return l
+}
+
+func (l *LRUDoubleList) DebugPrint() []int {
+	result := make([]int, 0)
+	p := l.Head
+	for p != nil {
+		result = append(result, p.Value)
+		p = p.next
+	}
+	return result
+}
+
+func (l *LRUDoubleList) AddToHead(node *LRUDoubleListNode) {
+	headNext := l.Head.next
+	l.Head.next = node
+	headNext.prev = node
+	node.next = headNext
+	node.prev = l.Head
+}
+func (l *LRUDoubleList) Remove(node *LRUDoubleListNode) {
+	node.prev.next = node.next
+	node.next.prev = node.prev
+}
+func (l *LRUDoubleList) RemoveTail() *LRUDoubleListNode {
+	if l.Head.next == l.Tail {
+		return nil
+	}
+	l.Remove(l.Tail.prev)
+	return l.Tail.prev
+}
+
+type LRUCache struct {
+	capacity  int
+	keymap    map[int]*LRUDoubleListNode
+	cacheList LRUDoubleList
+}
+
+func NewLRUCache(capacity int) LRUCache {
+	lru := LRUCache{capacity: capacity}
+	lru.keymap=make(map[int]*LRUDoubleListNode)
+	lru.cacheList = NewLRUDoubleList()
+	return lru
+}
+func (lru *LRUCache) makeRecently(key int) {
+	node := lru.keymap[key]
+	lru.cacheList.Remove(node)
+	lru.cacheList.AddToHead(node)
+}
+func (lru *LRUCache) addItem(key, Value int) {
+	node := &LRUDoubleListNode{Key: key, Value: Value}
+	lru.keymap[key]=node
+	lru.cacheList.AddToHead(node)
+}
+func (lru *LRUCache) removeItem(key int) {
+	node := lru.keymap[key]
+	lru.cacheList.Remove(node)
+	delete(lru.keymap, key)
+}
+func (lru *LRUCache) removeLeast() {
+	node := lru.cacheList.RemoveTail()
+	delete(lru.keymap, node.Key)
+}
+
+//func Constructor146(capacity int) LRUCache {
+//}
+//
+func (lru *LRUCache) Get(key int) int {
+	if item,ok:=lru.keymap[key];ok{
+		return item.Value
+	}else{
+		return -1
+	}
+
+}
+//
+func (lru *LRUCache) Put(key int, value int) {
+	if item,ok:=lru.keymap[key];ok{
+		item.Value=value
+		lru.makeRecently(item.Key)
+	}else{
+		if len(lru.keymap)>=lru.capacity{
+			lru.removeLeast()
+		}
+		lru.addItem(key,value)
+	}
+	fmt.Println(len(lru.keymap),lru.capacity,lru.keymap)
+}
+
--- src/main/main.go
+++ src/main/main.go
@@ -20,7 +20,14 @@ func main() {
 	t1.Right.Left=&algo.TreeNode{Val:6}
 	t1.Right.Right=&algo.TreeNode{Val:7}
 	print(t1,"\n")
-	fmt.Println(leetcode.Subsets0([]int{1,2,3}))
-	fmt.Println(leetcode.Subsets3([]int{1,2,3}))
-
+	l1:=leetcode.NewLRUCache(3)
+	l1.Put(9,19)
+	l1.Put(8,18)
+	l1.Put(7,17)
+	fmt.Println(l1.Get(9))
+	fmt.Println(l1.Get(8))
+	fmt.Println(l1.Get(7))
+	l1.Put(6,16)
+	l1.Put(5,15)
+	fmt.Println(l1.Get(9))
 }
