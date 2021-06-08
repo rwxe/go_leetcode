4a45fb55a804a8988ae59c663ae63b19d1cdc85f
yizdu <jzunk@protonmail.com>
2021-06-09 00:42:05 +0800

    *

--- src/leetcode/leetcode_JZOffer_32.go
+++ src/leetcode/leetcode_JZOffer_32.go
@@ -1,47 +1,47 @@
 package leetcode
 
+func reverseLevel(level []int) {
+	for i, j := 0, len(level)-1; i < j; i, j = i+1, j-1 {
+		level[i], level[j] = level[j], level[i]
+
+	}
+
+}
+
 func LevelOrderJZ32(root *TreeNode) [][]int {
+	result := make([][]int, 0)
 	if root == nil {
-		return nil
+		return result
 	}
 	queue := make([]*TreeNode, 0)
 	queue = append(queue, root)
-	res := make([][]int, 0)
-	left := true
 	for len(queue) > 0 {
+		levelWidth := len(queue)
 		level := make([]int, 0)
-		width := len(queue)
-		if left {
-			left = false
-			for i := 0; i < width; i++ {
-				node := queue[0]
-				queue = queue[1:]
-				level = append(level, node.Val)
-				if node.Left != nil {
-					queue = append(queue, node.Left)
-				}
-				if node.Right != nil {
-					queue = append(queue, node.Right)
-				}
-
+		for i := 0; i < levelWidth; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
 			}
-		} else {
-			left = true
-			for i := width; i > 0; i-- {
-				node := queue[0]
-				queue = queue[1:]
-				level = append(level, node.Val)
-				if node.Right != nil {
-					queue = append(queue, node.Right)
-				}
-				if node.Left != nil {
-					queue = append(queue, node.Left)
-				}
-
+			if node.Right != nil {
+				queue = append(queue, node.Right)
 			}
 		}
-		res = append(res, level)
+		result = append(result, level)
+	}
+	left := false
+	for i := range result {
+		if left {
+			left = false
+			reverseLevel(result[i])
+
+		} else {
+			left = true
+		}
 
 	}
-	return res
+	return result
+
 }
--- src/main/main.go
+++ src/main/main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-//	"go_leetcode/src/algo"
+  	"go_leetcode/src/algo"
 	"go_leetcode/src/leetcode"
 )
 
 func main() {
-//	t1:=&algo.TreeNode{Val:1}
-//	t1.Left=&algo.TreeNode{Val:2}
-//	t1.Right=&algo.TreeNode{Val:3}
-//	t1.Left.Left=&algo.TreeNode{Val:4}
-//	t1.Left.Right=&algo.TreeNode{Val:5}
-//	t1.Right.Left=&algo.TreeNode{Val:6}
-//	t1.Right.Right=&algo.TreeNode{Val:7}
-//	print(t1,"\n")
-	fmt.Println(leetcode.CountPrimes0(500000))
-	fmt.Println(leetcode.CountPrimes1(500000))
+  	t1:=&algo.TreeNode{Val:1}
+  	t1.Left=&algo.TreeNode{Val:2}
+  	t1.Right=&algo.TreeNode{Val:3}
+  	t1.Left.Left=&algo.TreeNode{Val:4}
+  	t1.Left.Right=&algo.TreeNode{Val:5}
+  	t1.Right.Left=&algo.TreeNode{Val:6}
+  	t1.Right.Right=&algo.TreeNode{Val:7}
+	fmt.Println(leetcode.LevelOrderJZ32(t1))
 }
