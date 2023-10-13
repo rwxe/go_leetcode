ff87f376ea1dfc7ef70be6f6f364a106e09ae4ba
rwxe <jzunk@protonmail.com>
2023-10-14 00:03:21 +0800

    *

--- src/leetcode/leetcode149.go
+++ src/leetcode/leetcode149.go
@@ -1,24 +1,107 @@
 package leetcode
 
-func k149(p1, p2 []int) float64 {
-	return float64(p2[1]-p1[1]) / float64(p2[0]-p1[0])
+func maxPoints(points [][]int) int {
+	type linearEquation struct {
+		//Ax+By+C=0
+		A, B, C int
+	}
+	gcd := func(m, n int) int {
+		for n != 0 {
+			m, n = n, m%n
+		}
+		return m
+	}
+	genPos := func(p []int) [2]int {
+		return [2]int{p[0], p[1]}
+	}
+	genLinearEquation := func(p1, p2 [2]int) linearEquation {
+		//Ax+By+C=0
+		//A:y2-y1
+		//B:x1-x2
+		//C:x2y1-x1y2
+		A := p2[1] - p1[1]
+		B := p1[0] - p2[0]
+		C := p2[0]*p1[1] - p1[0]*p2[1]
+		//自动处理符号位，不同顺序p1和p2将会生成同样的方程
+		AllGcd := gcd(gcd(A, B), gcd(B, C))
+		A /= AllGcd //让ABC互质
+		B /= AllGcd
+		C /= AllGcd
+		return linearEquation{A, B, C}
+	}
+	n := len(points)
+	if n <= 2 {
+		return n
+	}
+	leMap := make(map[linearEquation]map[[2]int]struct{})
+	maxCount := 1
+	for i := 0; i < n-1; i++ {
+		//优化，此时已经获得最大值
+		if maxCount >= n-i || maxCount > n/2 {
+			break
+		}
+		for j := i + 1; j < n; j++ {
+			pi, pj := genPos(points[i]), genPos(points[j])
+			le := genLinearEquation(pi, pj)
+			if leMap[le] == nil {
+				leMap[le] = make(map[[2]int]struct{})
+			}
+			leMap[le][pi] = struct{}{}
+			leMap[le][pj] = struct{}{}
+			if currCount := len(leMap[le]); currCount > maxCount {
+				maxCount = currCount
+			}
+		}
+	}
+	return maxCount
+
 }
 
-func MaxPoints(points [][]int) int {
-		maxLine := 0
-	for i, p1 := range points {
-		pointsWithoutP1 := append([][]int(nil), points[:i]...)
-		pointsWithoutP1 = append(pointsWithoutP1, points[i+1:]...)
-		kMap := make(map[float64]int)
-		for _, p2 := range pointsWithoutP1 {
-			kMap[k149(p1, p2)]++
-			if kMap[k149(p1, p2)] > maxLine {
-				maxLine = kMap[k149(p1, p2)]
-			}
-
-		}
-
+func MaxPoints_2(points [][]int) int {
+	n := len(points)
+	if n <= 2 {
+		return n
 	}
-	return maxLine+1
-
+	gcd := func(m, n int) int {
+		for n != 0 {
+			m, n = n, m%n
+		}
+		return m
+	}
+	abs := func(x int) int {
+		if x < 0 {
+			x = -x
+		}
+		return x
+	}
+	maxCount := 0
+	for i, p := range points {
+		//每一轮遍历，count map要重置，否值会将个平行的点一起加入
+		count := make(map[[2]int]int)
+		//优化，此时已经获得最大值
+		if maxCount >= n-i || maxCount > n/2 {
+			break
+		}
+		for _, q := range points[i+1:] {
+			x, y := p[0]-q[0], p[1]-q[1]
+			if x == 0 {
+				y = 1
+			} else if y == 0 {
+				x = 1
+			} else {
+				if y < 0 {
+					x, y = -x, -y
+				}
+				g := gcd(abs(x), abs(y))
+				x /= g
+				y /= g
+			}
+			k := [2]int{y, x}
+			count[k]++
+			if currCount := count[k] + 1; currCount > maxCount {
+				maxCount = currCount
+			}
+		}
+	}
+	return maxCount
 }
--- src/main/main.go
+++ src/main/main.go
@@ -2,39 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"runtime"
 	"src/leetcode"
-	"unsafe"
 )
 
-func printAllocMain() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-
-	fmt.Printf("%d MB\n", m.Alloc/1024/1024)
-}
 func main() {
-	var words = []string{}
-	for i := 'a'; i <= 'z'; i++ {
-		// 将 rune 转换为字符串并添加到切片中
-		words = append(words, string(i))
-	}
-	l := leetcode.Constructor359()
-	for i := 0; i < 100; i++ {
-		msg := words[rand.Intn(26)]
-		l.ShouldPrintMessageLRU(i, msg)
-	}
-	runtime.GC()
-	//单次测试的调试信息
-	B := *(*uint8)(unsafe.Pointer(uintptr(unsafe.Pointer(*(**int)(unsafe.Pointer(&l)))) + 9))
-	fmt.Println("\nthe hmap B size", B)
-	printAllocMain()
-
-	//	for k := range l.LogHist {
-	//		if delTimes, ok := l.DeletedKey[k]; ok {
-	//			fmt.Println("WTF", k, delTimes)
-	//		}
-	//	}
-
+	fmt.Println(leetcode.MaxPoints_2([][]int{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {1, -2}}))
 }
